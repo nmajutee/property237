dc6904dcfe8c51168a70b3a68688ae885ccdbe0d
nmaju <nmajutee@gmail.com>
2025-10-10 15:42:06 +0100

    fix: Remove 'storages' from INSTALLED_APPS (no longer needed, fixes deployment error)

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'storages',  # django-storages for AWS S3
+    # 'storages',  # removed: no longer using django-storages
     'rest_framework',
     'rest_framework_simplejwt',  # JWT Authentication
     'rest_framework_simplejwt.token_blacklist',  # JWT token blacklist
